ac79750dbd38a1aa73db58ac58357312bbd05a84
fullstackkdev <fullstackkdev@gmail.com>
2023-10-12 15:28:42 +0500

    store in redux only when user login successfully

--- src/screens/signIn/api/index.js
+++ src/screens/signIn/api/index.js
@@ -4,7 +4,7 @@ import { userActions } from "../../../Redux/store/userSlice";
 export const login = (payload) => async (dispatch) => {
   try {
     const response = await axios.post("/login/", payload);
-    dispatch(userActions.addUser(response.data.payload));
+    if(response.data.success) dispatch(userActions.addUser(response.data.payload));
     return response;
   } catch (error) {
     return error;
@@ -14,7 +14,7 @@ export const login = (payload) => async (dispatch) => {
 export const loginWith3rdParty = (payload) => async (dispatch) => {
   try {
     const response = await axios.post("/third-party-user-login/", payload);
-    dispatch(userActions.addUser(response.data.payload));
+    if(response.data.success) dispatch(userActions.addUser(response.data.payload));
     return response;
   } catch (error) {
     return error;
