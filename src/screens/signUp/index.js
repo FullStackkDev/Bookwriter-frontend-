ca4cf3de850b7c4cc2c4296b2e327f531a4bdf02
fullstackkdev <fullstackkdev@gmail.com>
2023-10-20 11:45:13 +0500

    Add code for navigating user to sign in page when successfully registered.

--- src/screens/signUp/index.js
+++ src/screens/signUp/index.js
@@ -6,6 +6,7 @@ import Design from "./design";
 import { showToast } from "../../helper/tosat";
 import { validateForm } from "../../utils/utils";
 import { validationRules } from "./validator/rules";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
   const [userData, setUserData] = useState({
@@ -17,6 +18,7 @@ function SignUp() {
     confirmPassword: "",
   });
   const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -45,6 +47,7 @@ function SignUp() {
               password: "",
               phoneNo: "",
             });
+            navigate("/signin");
           } else {
             setErrors(response.data.message.error);
           }
