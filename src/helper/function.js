87852482929337cd97bb6210da2ee890fbc7182e
fullstackkdev <fullstackkdev@gmail.com>
2023-10-20 17:21:28 +0500

    update handle 3rd Party Integrationfor payload

--- src/helper/function.js
+++ src/helper/function.js
@@ -12,40 +12,86 @@ export const handleChange = (event, stateData, setStateData) => {
   });
 };
 
-export const handle3rdPartyIntegration = (response, dispatch, provider) => {
-  let payload = {};
-  if (provider === "facebook") {
-    const { first_name, last_name, email, userID } = response;
-    payload = {
-      first_name: first_name,
-      last_name: last_name ? last_name : first_name,
-      email: email,
-      third_party_user_id: userID,
-      third_party_type: provider,
-    };
-  } else {
-    const { given_name, family_name, email, sub } = response;
-    payload = {
-      first_name: given_name,
-      last_name: family_name ? family_name : given_name,
-      email: email,
-      third_party_user_id: sub,
-      third_party_type: provider,
-    };
-  }
-  dispatch(loginWith3rdParty(payload))
-    .then((response) => {
-      if (!response.data.success) {
-        if (response.data.message.error) {
-          const errors = response.data.message.error;
-          const errorMessages = Object.values(errors).join(", ");
-          showToast(errorMessages, "error");
-        } else {
-          showToast(response.data.message, "error");
-        }
+function payloadProcessorFor3rdParty(response, provider) {
+  const payload = {};
+
+  if (response.name) {
+    // Check if there is a name in the response
+    const nameParts = response.name.split(/[ -]/); // Split name using whitespace or hyphen
+
+    // Save the first index into first_name and concatenate the rest into last_name
+    payload.first_name = nameParts[0];
+    payload.last_name = nameParts.slice(1).join(" ");
+
+    // If there's only one index, check for family_name or last_name in the response
+    if (nameParts.length === 1) {
+      if (response.family_name) {
+        payload.last_name = response.family_name;
+      } else if (response.last_name) {
+        payload.last_name = response.last_name;
+      } else {
+        payload.last_name = payload.first_name;
       }
-    })
-    .catch((error) => {
-      showToast(UNABLE_TO_CONTINUE, "error");
-    });
+    }
+  } else if (response.login) {
+    // Check if there is a login in the response (for GitHub)
+    const nameParts = response.login.split(/[ -]/); // Split name using whitespace or hyphen
+
+    // Save the first index into first_name and concatenate the rest into last_name
+    payload.first_name = nameParts[0];
+    payload.last_name = nameParts.slice(1).join(" ");
+
+    // If there's only one index, check for family_name or last_name in the response
+    if (nameParts.length === 1) {
+      if (response.family_name) {
+        payload.last_name = response.family_name;
+      } else if (response.last_name) {
+        payload.last_name = response.last_name;
+      } else {
+        payload.last_name = payload.first_name;
+      }
+    }
+  }
+
+  if (response.sub) {
+    // Check if response has sub, id, or userId and assign to third_party_user_id
+    payload.third_party_user_id = response.sub;
+  } else if (response.id) {
+    payload.third_party_user_id = response.id;
+  } else if (response.userId) {
+    payload.third_party_user_id = response.userId;
+  }
+
+  payload.third_party_type = provider; // Assign third_party_type as provider
+
+  if (response.email) {
+    // Check and assign email from the response
+    payload.email = response.email;
+  } else {
+    showToast("No email found! Need to add email.", "error"); // If email is null, show a toast and return an empty payload
+    return {};
+  }
+
+  return payload;
+}
+
+export const handle3rdPartyIntegration = (response, dispatch, provider) => {
+  const payload = payloadProcessorFor3rdParty(response, provider);
+  if (Object.keys(payload).length) {
+    dispatch(loginWith3rdParty(payload))
+      .then((response) => {
+        if (!response.data.success) {
+          if (response.data.message.error) {
+            const errors = response.data.message.error;
+            const errorMessages = Object.values(errors).join(", ");
+            showToast(errorMessages, "error");
+          } else {
+            showToast(response.data.message, "error");
+          }
+        }
+      })
+      .catch((error) => {
+        showToast(UNABLE_TO_CONTINUE, "error");
+      });
+  }
 };
