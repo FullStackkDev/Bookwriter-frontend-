637da07ade4cf07bb9a25ba3193fce108956f012
fullstackkdev <fullstackkdev@gmail.com>
2023-10-26 18:31:33 +0500

    update login logic

--- src/api/thirdPartyLogin.js
+++ src/api/thirdPartyLogin.js
@@ -1,11 +1,14 @@
-import { userActions } from "../Redux/store/userSlice";
 import axios from ".";
+import { authActions } from "../Redux/store/authSlice";
+import { getUser } from "../components/Navbar/api";
 
 export const loginWith3rdParty = (payload) => async (dispatch) => {
   try {
     const response = await axios.post("/user", payload);
-    if (response.data.success)
-      dispatch(userActions.addUser(response.data.payload));
+    if (response.data.success) {
+      dispatch(authActions.Login(response.data.payload));
+      dispatch(getUser(response.data.payload.token));
+    }
     return response;
   } catch (error) {
     return error;
--- src/screens/signIn/api/index.js
+++ src/screens/signIn/api/index.js
@@ -1,11 +1,15 @@
 import axios from "../../../api";
+import { getUser } from "../../../components/Navbar/api";
+import { authActions } from "../../../Redux/store/authSlice";
 import { userActions } from "../../../Redux/store/userSlice";
 
 export const login = (payload) => async (dispatch) => {
   try {
     const response = await axios.post("/login", payload);
-    if (response.data.success)
-      dispatch(userActions.addUser(response.data.payload));
+    if (response.data.success) {
+      dispatch(authActions.Login(response.data.payload));
+      dispatch(getUser(response.data.payload.token));
+    }
     return response;
   } catch (error) {
     return error;
