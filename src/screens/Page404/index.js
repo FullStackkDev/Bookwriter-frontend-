3d6ade038511cda17104c063febded25c67914ed
fullstackkdev <fullstackkdev@gmail.com>
2023-10-30 12:32:44 +0500

    Add Page404 screen

---
+++ src/screens/Page404/index.js
@@ -0,0 +1,12 @@
+// SignIn/index.js
+import React from "react";
+import Design from "./design";
+function Page404() {
+  return (
+    <div>
+      <Design />
+    </div>
+  );
+}
+
+export default Page404;
