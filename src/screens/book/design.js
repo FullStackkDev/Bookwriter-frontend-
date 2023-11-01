73f28dfdfeaa3710a2536e0986ad0127291262b3
fullstackkdev <fullstackkdev@gmail.com>
2023-11-01 16:06:56 +0500

    Add book route. Add book screen. Create design for book detail screen. Add styles to book detail screen design.

--- src/screens/book/design.js
+++ src/screens/book/design.js
@@ -3,7 +3,14 @@ import Section from "../../components/Section";
 import { Box, Button, Typography } from "@mui/material";
 import { styles } from "./style.js";
 
-const Design = ({ filterSections, handleAddMainSection }) => {
+const Design = ({
+  filterSections,
+  sections,
+  addMainSection,
+  deleteSection,
+  updateSection,
+  addSubSection,
+}) => {
   const {
     mainContainer,
     mainContainerBook,
@@ -13,59 +20,42 @@ const Design = ({ filterSections, handleAddMainSection }) => {
     AddSectionButtonContainer,
   } = styles;
   return (
-    <>
-      <Box sx={mainContainer}>
-        <Box sx={mainContainerBook}>
-          <Typography variant="h4" sx={bookTitle}>
-            BOOK TITLE
-          </Typography>
+    <Box sx={mainContainer}>
+      <Box sx={mainContainerBook}>
+        <Typography variant="h4" sx={bookTitle}>
+          BOOK TITLE
+        </Typography>
 
-          <Typography variant="h6" sx={bookAuthorDate}>
-            April 1, 2020 by Olivier
-          </Typography>
-          <Typography sx={bookDescription}>
-            This blog post shows a few different types of content that are
-            supported and styled with Material styles. Basic typography, images,
-            and code are all supported. You can extend these by modifying
-            Markdown.js. Cum sociis natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque
-            ornare sem lacinia quam venenatis vestibulum. Sed posuere
-            consectetur est at lobortis. Cras mattis consectetur purus sit amet
-            fermentum.
-          </Typography>
-        </Box>
-
-        <Box sx={AddSectionButtonContainer}>
-          <Button
-            variant={"contained"}
-            onClick={() => {
-              // HERE ONLY CALL HANDLER FUNCTION NO PARAMETERS WITH FUNCTION
-              const newSection = {
-                title: "TEST",
-                _id: `section-${Date.now()}-${Math.random()
-                  .toString(16)
-                  .slice(2)}`,
-                parent_section_id: null,
-                book_id: "Bio1",
-                content:
-                  "Description / Content This blog post shows a few different types of content that are supported and styled with Material styles. Basic typography, images, and code are all supported. You can extend these by modifying Markdown.js. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.",
-              };
-              handleAddMainSection(newSection);
-            }}
-          >
-            Add Section
-          </Button>
-        </Box>
-        {filterSections?.map((section) => (
-          <Section key={section._id} section={section} />
-        ))}
-        {filterSections.length === 0 && (
-          <Typography variant="h6" sx={bookTitle}>
-            NO SECTION FOUND PLEASE ADD SECTION
-          </Typography>
-        )}
+        <Typography variant="h6" sx={bookAuthorDate}>
+          April 1, 2020 by Olivier
+        </Typography>
+        <Typography paragraph sx={bookDescription}>
+          This blog post shows a few different types of content that are
+          supported and styled with Material styles. Basic typography, images,
+          and code are all supported. You can extend these by modifying
+          Markdown.js. Cum sociis natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque
+          ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur
+          est at lobortis. Cras mattis consectetur purus sit amet fermentum.
+        </Typography>
       </Box>
-    </>
+
+      <Box sx={AddSectionButtonContainer}>
+        <Button variant={"contained"} onClick={addMainSection}>
+          Add Section
+        </Button>
+      </Box>
+      {filterSections?.map((section) => (
+        <Section
+          key={section._id}
+          section={section}
+          sections={sections}
+          deleteSection={deleteSection}
+          updateSection={updateSection}
+          addSubSection={addSubSection}
+        />
+      ))}
+    </Box>
   );
 };
 
