6fd25cb7993e16d1a7656a65a58bbc9840078172
fullstackkdev <fullstackkdev@gmail.com>
2023-11-14 13:20:19 +0500

    Create a Modal for adding book.

---
+++ src/components/Modal/Book/AddBookModal/style.js
@@ -0,0 +1,17 @@
+// addNewBook/styles.js
+export const styles = {
+  modal: { display: "grid", placeItems: "center" },
+
+  icons: { cursor: "pointer" },
+
+  divider: { my: 0 },
+
+  boxForm: { mt: 1 },
+
+  imageTag: {
+    objectFit: "contain",
+    width: "500px",
+    height: "300px",
+  },
+  uploadImageButton: { width: "100%" },
+};
