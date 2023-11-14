fbf05f3513624767b33d0bdcb55ea14e8b013d22
fullstackkdev <fullstackkdev@gmail.com>
2023-11-14 14:02:32 +0500

    Implement redux for writerRole.

--- src/Redux/store/index.js
+++ src/Redux/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import authSlice from "./authSlice";
 import bookSlice from "./bookSlice";
+import writerRoleSlice from "./writerRoleSlice";
 
 export default configureStore({
   reducer: {
     user: userSlice.reducer,
     auth: authSlice.reducer,
     books: bookSlice.reducer,
+    writerRoles: writerRoleSlice.reducer,
   },
 });
